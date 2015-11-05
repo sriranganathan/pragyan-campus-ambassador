38a93649fc165b984f26ac191faf9e531bd9a70b
Kousik Satish <kousiksatish@gmail.com>
2015-11-05 09:23:58 +0530

    Facebook integrated, user table created

---
+++ database/migrations/2015_11_05_032914_Users.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id',100);
+            $table->string('facebook_user_id',30);
+            $table->string('full_name',150);
+            $table->string('image_url', 200);
+            $table->string('college', 200);
+            $table->string('city', 100);
+            $table->string('dept', 150);
+            $table->string('year', 6);
+            $table->string('mobile', 15);
+            $table->string('email', 300);
+            $table->string('por', 500);
+            $table->string('question', 1200);
+            $table->timestamp('created_at')
+                    ->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('users');
+    }
+}
